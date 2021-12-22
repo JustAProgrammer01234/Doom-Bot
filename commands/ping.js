08ebcd7b26faf6db79f131240194258c3f883f71
JustAProgrammer01234 <scriptoentity@gmail.com>
2021-12-22 11:16:16 +0800

    Removed a semicolon.

--- commands/ping.js
+++ commands/ping.js
@@ -3,6 +3,6 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
     data: new SlashCommandBuilder().setName("ping").setDescription("Sends the bot's websocket heartbeat."),
     execute: async (interaction) => {
-        await interaction.reply(`**PONG!** \`${interaction.client.ws.ping}ms\``);
+        await interaction.reply(`**PONG!** \`${interaction.client.ws.ping}ms\``)
     }
-};
+}
