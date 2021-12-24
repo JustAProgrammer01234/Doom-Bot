af0921ea01a493c7da95e315c235dcbe48926a40
JustAProgrammer01234 <scriptoentity@gmail.com>
2021-12-24 11:46:03 +0800

    Fixed a bug.

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 
 module.exports = {
-    data: SlashCommandBuilder().setName("botinfo").setDescription("Sends info about the bot (which is me btw)."),
+    data: new SlashCommandBuilder().setName("botinfo").setDescription("Sends info about the bot (which is me btw)."),
     execute: async (interaction) => {
         await interaction.reply("This command is still in development!")
     }
