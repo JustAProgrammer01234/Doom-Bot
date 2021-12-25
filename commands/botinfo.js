0376a6fc4f618a173243040adfaf06e1755b7e01
JustAProgrammer01234 <scriptoentity@gmail.com>
2021-12-25 20:05:37 +0800

    Changes :mhm:

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,13 +1,17 @@
-const { SlashCommandBuilder } = require("@discordjs/builders")
+const { SlashCommandBuilder, strikethrough } = require("@discordjs/builders")
 const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js")
 
 module.exports = {
     data: new SlashCommandBuilder().setName("botinfo").setDescription("Sends info about the bot (which is me btw)."),
     execute: async (interaction) => {
+        const doomBot = interaction.client
+         
         const botinfoEmbed = new MessageEmbed()
             .setTitle("Some info about me:")
-            .setDescription("Never gonna give you up.")
+            .setDescription(`Creator: ${strikethrough("Davoth")} ${doomBot.application.owner.username}
+            Started logging into discord at: ${Math.floor(doomBot.readyTimestamp / 1000)}`)
             .setColor("#FF0000")
+
         const row = new MessageActionRow()
             .addComponents(
                 new MessageButton()
@@ -16,6 +20,7 @@ module.exports = {
                     .setURL("https://github.com/JustAProgrammer01234/Doom-Bot")
                     .setEmoji("923803162148741130")
             )
-        await interaction.reply({ embeds: [botinfoEmbed], components: [row]})
+
+        await interaction.reply({ embeds: [botinfoEmbed], components: [row] })
     }
 }
