5a8e8867bf9ce78a4b56df4470d263621c6a12a7
JustAProgrammer01234 <scriptoentity@gmail.com>
2021-12-25 20:59:21 +0800

    Just made changes.

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -5,12 +5,15 @@ module.exports = {
     data: new SlashCommandBuilder().setName("botinfo").setDescription("Sends info about the bot (which is me btw)."),
     execute: async (interaction) => {
         const doomBot = interaction.client
+        const readyAt = Math.floor(doomBot.readyTimestamp / 1000)
 
         const botinfoEmbed = new MessageEmbed()
             .setTitle("Some info about me:")
             .setDescription(`Creator: ${strikethrough("Davoth")} ${doomBot.application.owner.username}
-            Started logging into discord at: ${time(Math.floor(doomBot.readyTimestamp / 1000), "F")}`)
+            Started logging into discord at: ${time(Math.floor(readyAt, "F"))} ${time(Math.floor(readyAt, "R"))}
+            Servers I'm in: ${doomBot.guilds}`)
             .setColor("#FF0000")
+            .setThumbnail(doomBot.user.avatarURL())
 
         const row = new MessageActionRow()
             .addComponents(
