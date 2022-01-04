51fed26bd74ccb0264e2fba4134643e40788ac64
JustAProgrammer01234 <scriptoentity@gmail.com>
2022-01-04 12:18:17 +0800

    Fix yes

--- doombot/utils.js
+++ doombot/utils.js
@@ -5,7 +5,7 @@ const cmdFiles = fs.readdirSync("./commands")
 
 function navigateCommands(func) {
     for (const dir of cmdFiles) {
-        const category = fs.readdirSync(`./commands${dir}`)
+        const category = fs.readdirSync(`./commands/${dir}`)
         for (const cmdFile of category) {
             func(category, cmdFile)
         }
@@ -16,7 +16,7 @@ function listCommands() {
     const commands = new Collection()
     const commandList = [] 
     for (const dir of cmdFiles) {
-        const category = fs.readdirSync(`./commands${dir}`)
+        const category = fs.readdirSync(`./commands/${dir}`)
         for (const cmdFile of category) {
             commandList.push([cmdFile.data.name, cmdFile.data.description])
         }
