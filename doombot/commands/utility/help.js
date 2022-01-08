a1239e16b51d81fc46b090daeca1350945c56e8b
JustAProgrammer01234 <scriptoentity@gmail.com>
2022-01-08 13:23:20 +0800

    yes

--- doombot/commands/utility/help.js
+++ doombot/commands/utility/help.js
@@ -28,13 +28,11 @@ module.exports = {
             .setOptions(options)
         const actionRow = new MessageActionRow().addComponents(helpSelectMenu)
         const message = await interaction.reply({ embeds: [ helpEmbed ], components: [ actionRow ] })
-        const filter = (i) => {i.deferUpdate(); return interaction.user.id === i.user.id}
+        const filter = (i) => {return interaction.user.id === i.user.id}
         const menuCollector = message.createMessageComponentSelector({ filter, componentType: "SELECT_MENU", time: 10000 })
 
         menuCollector.on("collect", async (i) => {
-            if (i.componentType == "SELECT_MENU") {
-                await i.editReply("yes")
-            }
+            await i.reply("Hey Scripto, better finish this help command.")
         })
         menuCollector.on("end", () => {
             helpSelectMenu.setDisabled(true)
