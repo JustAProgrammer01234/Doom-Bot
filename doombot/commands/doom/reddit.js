67dedbaf94e13b687120c7efac99818603a1c376
JustAProgrammer01234 <scriptoentity@gmail.com>
2022-01-11 11:50:44 +0800

    made some little changes

--- doombot/commands/doom/reddit.js
+++ doombot/commands/doom/reddit.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 
 module.exports = {
-    data: new SlashCommandBuilder().setName("reddit").setDescription("Sends a post in the r/Doom subreddit."),
+    data: new SlashCommandBuilder().setName("reddit").setDescription("Sends a post from the r/Doom subreddit."),
     execute: async (interaction) => {
         await interaction.reply("In development.")
     }
