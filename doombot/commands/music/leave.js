e5db83732170bc4881c5013a1d28ca7b3ba080c7
JustAProgrammer01234 <scriptoentity@gmail.com>
2022-01-14 13:01:39 +0800

    embed fix

--- doombot/commands/music/leave.js
+++ doombot/commands/music/leave.js
@@ -13,7 +13,7 @@ module.exports = {
             "Please don't make me leave! There's more music to play!",
             "Let's beat it -- This is turning into a bloodbath!",
             "I wouldn't make myself leave if I were you. Other music bots are much worse.",
-            "Don't leave yet -- There's so much things that we can do here!",
+            "Don't make me leave yet -- There's so much things that we can do here!",
             "Ya know, next time you make me join a vc, I'm gonna toast ya.",
             "Go ahead and make me leave. See if I care.",
             "Are you sure you want to quit this great experience?"
@@ -31,6 +31,7 @@ module.exports = {
         const leaveEmbed = new MessageEmbed()
             .setTitle("Wanna quit?")
             .setDescription(exitMessage)
+            .setColor("#FF0000")
         const message = await interaction.reply({ embeds: [ leaveEmbed ], components: [ buttons ], fetchReply: true })
         const buttonCollector = message.createMessageComponentCollector({ componentType: "BUTTON" })
 
