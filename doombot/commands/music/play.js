2f7b48b224c32173693220121a929a35c1349383
JustAProgrammer01234 <scriptoentity@gmail.com>
2022-01-14 15:05:00 +0800

    bugfix

--- doombot/commands/music/play.js
+++ doombot/commands/music/play.js
@@ -2,11 +2,7 @@ const { SlashCommandBuilder } = require("@discordjs/builders")
 
 module.exports = {
     data: new SlashCommandBuilder().setName("play").setDescription("Tells the bot to play some damn music in vc.")
-        .addStringOption(option => {
-            option.setName("query")
-                .setDescription("The song to search for.")
-                .setRequired(true)
-        }),
+        .addStringOption((option) => option.setName("query").setDescription("The song to search for.").setRequired(true)),
     execute: async (interaction, assets) => {
         const botVoice = interaction.guild.me.voice.channel
         const voiceChannel = interaction.member.voice.channel
